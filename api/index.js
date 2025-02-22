af124e101a0fa9dd1b610e00206a6dcd41554816
Ehsan Ahmad <ehsanahmadahmadahmad@gmail.com>
2025-02-22 13:37:32 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('App listening on port 3000!');
+  console.log("App listening on port 3000!");
 });
